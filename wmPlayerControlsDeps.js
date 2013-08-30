a889c8adbd3f371513da9bed59fde47b8c12bb1b
Paolo Vergori <vergori@ismb.it>
2013-08-30 15:53:10 +0200

    Final modifications for smooth compilation

--- wmPlayerControlsDeps.js
+++ wmPlayerControlsDeps.js
@@ -1,13 +1,16 @@
 if(process.platform == 'win32'){
     fs = require('fs');
     var exec = require('child_process').exec;
-    fs.exists('build\\Release\\mediaWMplayerControls.node', function(exists){
-        if(exists){
-            console.log('mediaWMplayerControls.node found! Copying to node_modules/');
-            exec('copy build\\Release\\mediaWMplayerControls.node node_modules');            
-        }
+    fs.exists('node_modules\\mediaWMplayerControls.node', function(exists){
+        if(exists)
+            console.log('mediaWMplayerControls.node found!');            
         else{
             console.log('mediaWMplayerControls.node not found! proceeding with fresh compilation');
+            console.log('Required dependencies:');
+            console.log('\tC:/WinDDK/7600.16385.1/inc/atl71');
+            console.log('\tC:/WinDDK/7600.16385.1/lib/ATL/i386/atls.lib');
+            console.log('\tC:/WinDDK/7600.16385.1/lib/ATL/i386/atlsd.lib');
+            console.log('\tC:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include');
             exec('node-gyp configure build',function (error, stdout, stderr) {
                 console.log('---stdout---\n' + stdout + '\n');
                 if (error !== null) 
