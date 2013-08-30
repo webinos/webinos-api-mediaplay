75eab880944771f576993be55df0f14b3cb48491
Paolo Vergori <vergori@ismb.it>
2013-08-30 17:59:22 +0200

    test 3

--- src/wmPlayerControlsDeps.js
+++ src/wmPlayerControlsDeps.js
@@ -2,8 +2,14 @@ if(process.platform == 'win32'){
     fs = require('fs');
     var exec = require('child_process').exec;
     fs.exists('src\\build\\Release\\mediaWMplayerControls.node', function(exists){
-        if(exists)
-            console.log('mediaWMplayerControls.node found!');            
+        if(exists){
+            console.log('mediaWMplayerControls.node found!');
+            exec('xcopy \/Y build\\Release\\mediaWMplayerControls.node ..\\node_modules\\',function (error, stdout, stderr) {
+                console.log('---stdout---\n' + stdout + '\n');
+                if (error !== null) 
+                    console.log('\nexec error: ' + error);
+            });
+        }   
         else{
             console.log('mediaWMplayerControls.node not found! proceeding with fresh compilation');
             console.log('Required dependencies:');
